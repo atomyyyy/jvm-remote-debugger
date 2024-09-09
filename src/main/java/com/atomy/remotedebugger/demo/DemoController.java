d2a68d238564bafb3494aaeca75532531ed4304e
atomyyyy <atomyuen95@gmail.com>
2024-09-08 23:24:07 -0700

    feat: implement demo endpoint to test remote debugger

---
+++ src/main/java/com/atomy/remotedebugger/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.atomy.remotedebugger.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
+	}
+}
