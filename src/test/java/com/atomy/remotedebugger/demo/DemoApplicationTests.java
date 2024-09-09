862fe7dc09f56c57ab13522c9bb642bf688ed257
atomyyyy <atomyuen95@gmail.com>
2024-09-08 23:09:53 -0700

    chore: init repo

---
+++ src/test/java/com/atomy/remotedebugger/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomy.remotedebugger.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
